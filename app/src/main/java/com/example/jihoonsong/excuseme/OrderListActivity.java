697e8bf55fbde1450a8755a439c4c738a9e9828c
Jihoon Song <jihoonsong@Jihoonui-MacBook-Pro.local>
2017-01-21 22:57:40 +0900

    Implemented OrderListActivity

--- app/src/main/java/com/example/jihoonsong/excuseme/OrderListActivity.java
+++ app/src/main/java/com/example/jihoonsong/excuseme/OrderListActivity.java
@@ -1,10 +1,15 @@
 package com.example.jihoonsong.excuseme;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class OrderListActivity extends Activity {
+    ImageView orderConfirm;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         // Lock rotation.
@@ -12,5 +17,19 @@ public class OrderListActivity extends Activity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_list);
+
+        orderConfirm = (ImageView)findViewById(R.id.button_order_confirm_active);
+
+        orderConfirm.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (v.getId() == R.id.button_order_confirm_active) {
+                    startActivity(new Intent(OrderListActivity.this, OrderHistoryActivity.class));
+
+                    overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                    finish();
+                    System.gc();
+                }
+            }
+        });
     }
 }
