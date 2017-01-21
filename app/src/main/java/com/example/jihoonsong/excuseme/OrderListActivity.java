41a00fd3dfbbe199f053d79c02359ad64ffade42
Jihoon Song <jihoonsong@Jihoonui-MacBook-Pro.local>
2017-01-22 05:04:18 +0900

    Completed mock-up

--- app/src/main/java/com/example/jihoonsong/excuseme/OrderListActivity.java
+++ app/src/main/java/com/example/jihoonsong/excuseme/OrderListActivity.java
@@ -3,12 +3,19 @@ package com.example.jihoonsong.excuseme;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.graphics.Typeface;
+import android.media.Image;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class OrderListActivity extends Activity {
     ImageView orderConfirm;
+    ImageView shadow;
+    TextView orderConfirmMesg;
+    ImageView orderConfirmMesgBg;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,15 +26,28 @@ public class OrderListActivity extends Activity {
         setContentView(R.layout.activity_order_list);
 
         orderConfirm = (ImageView)findViewById(R.id.button_order_confirm_active);
+        shadow = (ImageView)findViewById(R.id.shadow);
+        orderConfirmMesg = (TextView)findViewById(R.id.order_confirm_mesg);
+        orderConfirmMesgBg = (ImageView)findViewById(R.id.order_confirm_mesg_bg);
+        orderConfirmMesg.setTypeface(Typeface.createFromAsset(getAssets(), "HU들꽃140.ttf"));
 
         orderConfirm.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 if (v.getId() == R.id.button_order_confirm_active) {
-                    startActivity(new Intent(OrderListActivity.this, OrderHistoryActivity.class));
+                    shadow.setVisibility(View.VISIBLE);
+                    orderConfirmMesg.setVisibility(View.VISIBLE);
+                    orderConfirmMesgBg.setVisibility(View.VISIBLE);
 
-                    overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-                    finish();
-                    System.gc();
+                    Handler mHandler = new Handler();
+                    mHandler.postDelayed(new Runnable() {
+                        public void run() {
+                            startActivity(new Intent(OrderListActivity.this, OrderHistoryActivity.class));
+
+                            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                            finish();
+                            System.gc();
+                        }
+                    }, 1100);
                 }
             }
         });
